48b10dd4d4aba0aad4ac64e914d456ac760f8100
lutaevaem <lutaevaem@gmail.com>
2026-05-18 10:42:01 +0700

    Route admin page from frontend entrypoint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
 import App from './App.jsx';
+import Admin from './Admin.jsx';
+
+const isAdminPage = window.location.pathname.startsWith('/admin');
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isAdminPage ? <Admin /> : <App />}
   </React.StrictMode>
 );
