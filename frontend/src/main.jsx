459f8866f3009a7e0001a3627da86fd741b95dfa
lutaevaem <lutaevaem@gmail.com>
2026-06-04 18:01:45 +0700

    Use upgraded admin with image upload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
 import App from './App.jsx';
-import Admin from './Admin.jsx';
+import Admin from './Admin2.jsx';
 
 const isAdminPage = window.location.pathname.startsWith('/admin');
 
